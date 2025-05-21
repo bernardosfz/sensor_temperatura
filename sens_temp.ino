8ca012c35a8e2378579e21729a71036e0496abf4
Jean F. Vanz <132222224+jeanvanz@users.noreply.github.com>
2025-05-20 22:05:54 -0300

    Arquivo do sensor de temperatura do arduino

---
+++ sens_temp.ino
@@ -0,0 +1,25 @@
+#include <dht11.h>  
+
+#define DHTPIN 2
+dht11 DHT11;
+
+#define LEDPIN 10
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LEDPIN, OUTPUT);
+}
+
+void loop() {
+  DHT11.read(DHTPIN);
+
+  Serial.println(DHT11.temperature);
+
+  if (DHT11.temperature < 15 || DHT11.temperature > 25) {
+    digitalWrite(LEDPIN, HIGH);
+  } else {
+    digitalWrite(LEDPIN, LOW);
+    }
+  delay(10000);
+}
